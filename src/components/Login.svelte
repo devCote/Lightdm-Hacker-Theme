e6310ba302cb4d0e19d2a86f895cda3f64b0d29c
devcote <bahanenko@gmail.com>
2023-08-14 02:24:05 +0300

    working version 1.0

--- src/components/Login.svelte
+++ src/components/Login.svelte
@@ -2,34 +2,17 @@
   import { slide } from 'svelte/transition'
   import { isAuthenticationError, isAuthenticated } from '../store'
   import lightdm from '../constants/lightdm'
-  import Form from './Form.svelte'
-    import { getLocalStorage, setLocalStorage } from 'src/helpers/functions'
-    import { DEFAULT_SESSION } from 'src/constants/variables'
 
   const username = 'devcote'
   let password = ''
-  let sessions = lightdm.sessions
-  let currentSession = getLocalStorage(DEFAULT_SESSION) || sessions[0].key;
 
-  const handleSession = () => {
-    const currentSessionId = sessions.indexOf(currentSession)
-    const totalSessions = sessions.lenght
-
-    let newSessionId = currentSessionId + 1
-    if (newSessionId >= totalSessions) newSessionId = 0
-
-    currentSession = sessions[newSessionId].key
-    setLocalStorage(DEFAULT_SESSION, currentSession)
-  }
-
-  const handleOnSubmit = () => {
+  function handleOnSubmit() {
     lightdm.cancel_authentication()
     lightdm.authenticate(username)
     setTimeout(() => {
       lightdm.respond(password)
     }, 1000)
   }
-
 </script>
 
 <div class="main_container flex flex-col items-start justify-center">
@@ -38,9 +21,18 @@
   {:else if $isAuthenticated}
     <h1 transition:slide class="message">ACCESS GRANTED</h1>
   {:else}
-    <Form {handleOnSubmit} {password} />
+    <form on:submit|preventDefault={handleOnSubmit}>
+      <h1 class="user">Devcote</h1>
+      <!-- svelte-ignore a11y-autofocus -->
+      <input
+        type="password"
+        placeholder="Password"
+        bind:value={password}
+        autocomplete="off"
+        autofocus
+      />
+    </form>
     <!-- svelte-ignore a11y-click-events-have-key-events -->
-    <h1 on:click={handleSession} class="session">{currentSession}</h1>
   {/if}
 </div>
 
@@ -53,17 +45,31 @@
     padding: 30px 50px;
   }
 
-  .session {
-    cursor: pointer;
-    align-self: flex-end; 
-    color: #0f0;
-    font-weight: bold;
-  }
-
   .message {
     color: #0f0;
     font-weight: bold;
     letter-spacing: 2px;
     font-size: 2em;
   }
+
+    h1 {
+    color: #0f0;
+    font-weight: bold;
+    letter-spacing: 2px;
+    font-size: 2em;
+  }
+
+  input {
+    color: #0f0;
+    font-size: 1.5em;
+    border: transparent;
+    background: transparent !important;
+    outline: none;
+    margin-top: 10px;
+  }
+
+  input::placeholder {
+    color: #0f0;
+  }
+
 </style>
