7fd69a1deeb799dc44f26756e4d27cd100616e47
devcote <bahanenko@gmail.com>
2023-08-14 00:20:13 +0300

    latest patch

--- src/components/Login.svelte
+++ src/components/Login.svelte
@@ -1,21 +1,35 @@
 <script>
   import { slide } from 'svelte/transition'
-
   import { isAuthenticationError, isAuthenticated } from '../store'
   import lightdm from '../constants/lightdm'
   import Form from './Form.svelte'
+    import { getLocalStorage, setLocalStorage } from 'src/helpers/functions'
+    import { DEFAULT_SESSION } from 'src/constants/variables'
 
-  let username = 'devcote'
+  const username = 'devcote'
   let password = ''
   let sessions = lightdm.sessions
+  let currentSession = getLocalStorage(DEFAULT_SESSION) || sessions[0].key;
 
-  let handleOnSubmit = () => {
+  const handleSession = () => {
+    const currentSessionId = sessions.indexOf(currentSession)
+    const totalSessions = sessions.lenght
+
+    let newSessionId = currentSessionId + 1
+    if (newSessionId >= totalSessions) newSessionId = 0
+
+    currentSession = sessions[newSessionId].key
+    setLocalStorage(DEFAULT_SESSION, currentSession)
+  }
+
+  const handleOnSubmit = () => {
     lightdm.cancel_authentication()
     lightdm.authenticate(username)
     setTimeout(() => {
       lightdm.respond(password)
     }, 1000)
   }
+
 </script>
 
 <div class="main_container flex flex-col items-start justify-center">
@@ -25,11 +39,8 @@
     <h1 transition:slide class="message">ACCESS GRANTED</h1>
   {:else}
     <Form {handleOnSubmit} {password} />
-    <ul>
-      {#each sessions as session}
-        <li>{session.key}</li>
-      {/each}
-    </ul>
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <h1 on:click={handleSession} class="session">{currentSession}</h1>
   {/if}
 </div>
 
@@ -40,6 +51,13 @@
     border: 1px solid #0f0;
     backdrop-filter: blur(10px); /* Blur effect */
     padding: 30px 50px;
+  }
+
+  .session {
+    cursor: pointer;
+    align-self: flex-end; 
+    color: #0f0;
+    font-weight: bold;
   }
 
   .message {
